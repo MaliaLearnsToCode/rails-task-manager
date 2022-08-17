2cd9560be1d85956ee8cdb691279d90ab399ebda
Malia Nguyen <dr.malia.nguyen@gmail.com>
2022-08-17 13:39:03 +0800

    Rails Task Manager code before refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # KEEP THE CORRECT SEQUENCE!
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: :new
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update', as: :update
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete
+
+  # resources :tasks
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
